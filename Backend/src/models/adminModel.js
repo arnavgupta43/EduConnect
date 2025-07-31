b0ee84bc6e714d4bb0cd7abceb7ab408da889af1
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-07-31 14:25:01 +0530

    add models

---
+++ Backend/src/models/adminModel.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const adminSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      unique: [true, "username should be unique"],
+      trim: true,
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      match: /^\S+@\S+\.\S+$/,
+    },
+    passwordHash: {
+      type: String,
+      required: [true, "password is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("passwordHash")) return next();
+  this.passwordHash = await bcryptjs.hash(this.passwordHash, 12);
+  next();
+});
+
+adminSchema.methods.matchPassword = function (enteredpassword) {
+  return bcryptjs.compare(enteredpassword, this.passwordHash);
+};
+
+adminSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { username: this.username, role: "admin", _id: this._id },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "2d",
+    }
+  );
+};
+adminSchema.methods.getName = function () {
+  return this.username;
+};
+
+module.exports = mongoose.model("Admin", adminSchema);
---
+++ Backend/src/models/teacherModel.js
@@ -0,0 +1,82 @@
+import mongoose, { model } from "mongoose";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// Teacher Model
+const teacherSchema = new mongoose.Schema(
+  {
+    username: {
+      unique: true,
+      required: [true, "Provide a unique username"],
+      type: String,
+      trim: true,
+      index: true,
+    },
+    name: {
+      type: String,
+      required: [true, "name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "Email should be unique"],
+      lowercase: true,
+      match: /^\S+@\S+\.\S+$/,
+    },
+    passwordHash: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    profile: {
+      photo: {
+        type: String,
+        default: "",
+      },
+      age: {
+        type: Number,
+        required: [true, "Age is required"],
+        min: 18,
+      },
+      mobileNo: {
+        type: String,
+        required: [true, "Number is required"],
+      },
+      address: {
+        type: String,
+        default: "Christ University Bengaluru",
+      },
+      previousExperience: String,
+      researchInterests: [String],
+      publications: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+teacherSchema.pre("save", async function (next) {
+  if (!this.isModified("passwordHash")) next();
+  this.passwordHash = await bcryptjs.hash(this.passwordHash, 10);
+  next();
+});
+
+teacherSchema.methods.matchPassword = function (enteredpassword) {
+  return bcryptjs.compare(enteredpassword, this.passwordHash);
+};
+
+teacherSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { username: this.username, role: "teacher", _id: this._id },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+};
+teacherSchema.methods.getName = function () {
+  return this.username;
+};
+
+module.exports = mongoose.model("Teacher", teacherSchema);
