e33959175e8c5a96a806fabe78de225224df18e5
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-08-11 00:38:12 +0530

    update app and server

---
+++ Backend/src/app.js
@@ -0,0 +1,32 @@
+require("dotenv").config(); // 1. Load env first
+const express = require("express");
+const app = express();
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const ErrorHandler = require("./middlewares/errorHandler");
+const loginRoute = require("./routes/auth");
+const morgan = require("morgan");
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10000,
+  message: {
+    status: "Fail",
+    msg: "Too many requests, please try again later.",
+  },
+});
+app.use(limiter);
+app.use(morgan("combined"));
+app.use(express.json()); // parse JSON bodies
+app.use(express.urlencoded({ extended: true }));
+app.use(ErrorHandler);
+app.use("/login", loginRoute);
+module.exports = app;
---
+++ Backend/src/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const app = require("./app");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 3000;
+const fs = require("fs");
+const path = require("path");
+//create a assets/temp
+const tempDir = path.join(__dirname, "assets", "temp");
+
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
+const start = async () => {
+  await connectDB(process.env.MONGO_URL);
+  console.log("MongoDB connected");
+  app.listen(PORT, () => console.log(`Server on ${PORT}`));
+};
+
+start();
