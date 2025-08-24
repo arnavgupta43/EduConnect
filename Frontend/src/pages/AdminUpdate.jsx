317d07cc7a99b2cb1492430eb9ef0006fef1bb4e
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-08-24 20:07:24 +0530

    update admin page

--- Frontend/src/pages/AdminUpdate.jsx
+++ Frontend/src/pages/AdminUpdate.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import AdminHeader from "../components/AdminDashboard/AdminHeader";
 import EditTeacherForm from "../components/AdminDashboard/AdminEditForm";
 import axios from "axios";
 import toast from "react-hot-toast";
@@ -53,6 +54,7 @@ const AdminUpdate = () => {
 
   return (
     <>
+      <AdminHeader />
       {teacher ? (
         <EditTeacherForm teacher={teacher} onSubmit={handleUpdateTeacher} />
       ) : (
